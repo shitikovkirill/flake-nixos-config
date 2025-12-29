01047833f5f144e1d9771a1d9c18494a9966ef02
Shitikov Kirill <shitikovkirillt@gmail.com>
2025-12-29 06:50:40 +0100

    Add plasma desctop

---
+++ desctop/plasma6.nix
@@ -0,0 +1,11 @@
+{
+  services.xserver.enable = true; # optional
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    plasma-browser-integration
+    konsole
+    elisa
+  ];
+}
